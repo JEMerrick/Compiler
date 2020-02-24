011e6fd81eec58cbe666ecae58f46f630e7c79e4
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-02-24 11:33:52 +0000

    Update ast.hpp

--- include/ast.hpp
+++ include/ast.hpp
@@ -1,11 +1,17 @@
 #ifndef H_AST
 #define H_AST
 
+#include "ast/ast_arithOps.hpp"
+#include "ast/ast_assignOps.hpp"
 #include "ast/ast_base.hpp"
+#include "ast/ast_bitOps.hpp"
+#include "ast/ast_conditions.hpp"
+#include "ast/ast_condOps.hpp"
+#include "ast/ast_funcs.hpp"
+#include "ast/ast_logicOps.hpp"
+#include "ast/ast_loops.hpp"
 #include "ast/ast_primitives.hpp"
-#include "ast/ast_operators.hpp"
 #include "ast/ast_unary.hpp"
-#include "ast/ast_arithmetic.hpp"
 
 
 extern const Base *parseAST();
