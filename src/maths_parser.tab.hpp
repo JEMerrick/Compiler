1f3444983a7e174a058c83d57381c82a8d11565e
Joanna <joannaemerrick@gmail.com>
2020-02-27 12:15:50 +0000

    fixing shiet

--- src/maths_parser.tab.hpp
+++ src/maths_parser.tab.hpp
@@ -39,6 +39,22 @@
 #if YYDEBUG
 extern int yydebug;
 #endif
+/* "%code requires" blocks.  */
+#line 1 "src/maths_parser.y" /* yacc.c:1909  */
+
+  #include "ast.hpp"
+
+  #include <cassert>
+
+  extern const Base *g_root; // A way of getting the AST out
+
+  //! This is to fix problems when generating C++
+  // We are declaring the functions provided by Flex, so
+  // that Bison generated code can call them.
+  int yylex(void);
+  void yyerror(const char *);
+
+#line 58 "src/maths_parser.tab.hpp" /* yacc.c:1909  */
 
 /* Token type.  */
 #ifndef YYTOKENTYPE
@@ -112,13 +128,13 @@ extern int yydebug;
 
 union YYSTYPE
 {
-#line 3 "src/maths_parser.y" /* yacc.c:1909  */
+#line 17 "src/maths_parser.y" /* yacc.c:1909  */
 
   const Base *expr;
   double number;
   std::string *string;
 
-#line 122 "src/maths_parser.tab.hpp" /* yacc.c:1909  */
+#line 138 "src/maths_parser.tab.hpp" /* yacc.c:1909  */
 };
 
 typedef union YYSTYPE YYSTYPE;
