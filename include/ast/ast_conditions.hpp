0e4507848ffc614a51a49e7aadef20ce8aa96050
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-03-17 17:18:49 +0000

    Update ast_conditions.hpp

--- include/ast/ast_conditions.hpp
+++ include/ast/ast_conditions.hpp
@@ -76,8 +76,26 @@ public:
     virtual void printMIPS (std::string reg, std::ostream &out) const override{
     }
     virtual void printC (std::ostream &out) const override{
+      out << "if (";
+      condition->printC(out);
+      out << ") \n";
+      out << "{";
+      branch->printC(out);
+      out << " } else {\n";
+      elseBranch->printC(out);
+      out << std::endl;
+      out << "}"
     }
     virtual void printPy (std::ostream &out) const override{
+      out << "if (";
+      condition -> printPy(out);
+      out << ") :\n";
+      out << "  ";
+      branch -> printPy(out);
+      out << std::endl;
+      out << "else :\n";
+      elseBranch->printPy(out);
+      out << std::endl;
     }
 };
 #endif
