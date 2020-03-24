7b173fafef7ba8beed0ebb41aaa87706916d7082
Joanna <joannaemerrick@gmail.com>
2020-03-24 12:06:05 +0000

    fixing george's spaggheti code

--- include/ast/ast_base.hpp
+++ include/ast/ast_base.hpp
@@ -8,25 +8,6 @@
 
 #include <memory>
 
-int count = 0;
-std::vector <int> regFlag {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // can't use $0-$7, $8-$25 free
-
-int findreg(){
-    int i = 8;
-    while(regFlag[i] != 0){
-      i++;
-    }
-    regFlag[i] = 1;
-    return i;
-}
-
-int indent = 0;
-
-std::string makelabel(){
-    return "L_" + std::to_string(count++);
-}
-
 class Base;
 
 typedef const Base *BasePtr;
@@ -42,6 +23,26 @@ class Base {
         virtual void printMIPS (std::string reg, std::ostream &out) const = 0;
         virtual void printC (std::ostream &out) const = 0;
         virtual void printPy (std::ostream &out) const = 0;
+        
+        int abcdef = 0;
+        std::vector <int> regFlag {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // can't use $0-$7, $8-$25 free
+
+        int findreg(){
+            int i = 8;
+            while(regFlag[i] != 0){
+            i++;
+            }
+            regFlag[i] = 1;
+            return i;
+        }
+
+        int indent = 0;
+
+        std::string makelabl(){
+            return "L_" + std::to_string(abcdef++);
+        }
+
 
 };
 
