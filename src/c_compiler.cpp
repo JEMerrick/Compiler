badcc18bb7e5e2189e661547ec6592b001022ba3
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-03-24 15:50:49 +0000

    Update c_compiler.cpp

--- src/c_compiler.cpp
+++ src/c_compiler.cpp
@@ -5,9 +5,8 @@ int main()
 {
     const Base* ast = parseAST();
     MIPZ myMIPS;
-    ast->printMIPS(std::cout, myMIPS);
+    ast->printMIPS("Hi", std::cout, myMIPS);
     std::cout<<std::endl;
 
     return 0;
 }
-
