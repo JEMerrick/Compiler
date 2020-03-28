e6831fa567019c7cd05ed02fa18e54564a9b9eca
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-03-28 12:46:48 +0000

    Update c_compiler.cpp

--- src/c_compiler.cpp
+++ src/c_compiler.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <fstream>
 #include "ast.hpp"
 
 int main(int argc, char *argv[]){
     const Base* ast = parseAST();
     ast -> printC(std::cout);
     std::cout << std::endl;
+    std::fstream fs;
+    fs.open(argv[2]);
 
     if(static_cast<std::string>(argv[1]) == "--translate"){
+        ofstream outfile;
+        outfile.open(argv[4]);
         Py myPython;
-        ast->printPy(std::cout, myPython);
+        ast->printPy(outfile, myPython);
         std::cout << std::endl;
+        outfile.close();
     }
 
     else if (static_cast<std::string>(argv[1]) == "-S"){
