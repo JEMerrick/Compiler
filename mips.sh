fd8b346bf4bc832fb498b8dc46bc601f988ba06c
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-03-30 12:02:52 +0100

    Update mips.sh

--- mips.sh
+++ mips.sh
@@ -16,28 +16,28 @@ for DRIVER in compiler_tests/array/*_driver.c ; do
     >&2 echo "Test case $NAME"
 
     # Compile driver with normal GCC
-    mips-linux-gnu-gcc -c $DRIVER -o working/${NAME}_driver.o 2> working/${NAME}_driver.compile.stderr
+    bin/c_compiler -S ${NAME}.c -o ${NAME}.s
     if [[ $? -ne 0 ]]; then
         >&2 echo "ERROR : Couldn't compile driver program using GCC."
         continue
     fi
 
     # Compile test function with compiler under test to assembly
-    cat $TESTCODE | $COMPILER > working/$NAME.s  2> working/${NAME}.compile.stderr
+    mips-linux-gnu-gcc -mfp32 -o ${NAME}.o -c ${NAME}.s
     if [[ $? -ne 0 ]]; then
         >&2 echo "ERROR : Compiler returned error message."
         continue
     fi
 
     # Link driver object and assembly into executable
-    mips-linux-gnu-gcc -static working/${NAME}.s working/${NAME}_driver.o -o working/${NAME}.elf 2> working/${NAME}.link.stderr
+    mips-linux-gnu-gcc -mfp32 -static -o ${NAME} ${NAME}.o ${NAME}_driver.c
     if [[ $? -ne 0 ]]; then
         >&2 echo "ERROR : Linker returned error message."
         continue
     fi
 
     # Run the actual executable
-    qemu-mips working/${NAME}.elf
+    qemu-mips ${NAME}
     if [[ $? -ne 0 ]]; then
         >&2 echo "ERROR : Testcase returned $?, but expected 0."
     else
