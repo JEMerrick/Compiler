0eef219ca32dda7ad133d957ac65ec5ce856c995
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-03-31 13:52:35 +0100

    Update ast_arithOps.hpp

--- include/ast/ast_arithOps.hpp
+++ include/ast/ast_arithOps.hpp
@@ -41,11 +41,16 @@ public:
     {}
 
     virtual void printMIPS (std::string reg, std::ostream &out, MIPZ &help) const override{
-      std::string r1 = "$" + std::to_string(help.parameters+1);
-      left->printMIPS(r1, out, help);
-      right->printMIPS(reg, out, help);
-      out << "ADDU " << reg << ", " << r1 << ", " << reg << std::endl;
-      help.regFlag[std::stoi(r1.substr(1))] = 0;
+        std::string r1 = "$" + std::to_string(help.findreg());
+        left->printMIPS(r1, out, help);
+        //ADDI RX 0, 5
+        std::string r2 = "$" + std::to_string(help.findreg());
+        right->printMIPS(r2, out, help);
+        //ADDI RY 0, 5
+        out << "ADDU " << reg << ", " << r1 << ", " << r2 << std::endl;
+        //ADDU RZ RY RX
+        help.regFlag[std::stoi(r1.substr(1))] = 0;
+        help.regFlag[std::stoi(r2.substr(1))] = 0;
     }
     virtual void printC (std::ostream &out) const override{
         out << "( ";
@@ -70,11 +75,13 @@ public:
     {}
 
     virtual void printMIPS (std::string reg, std::ostream &out, MIPZ &help) const override{
-      std::string r1 = "$" + std::to_string(help.parameters+1);
-      left->printMIPS(r1, out, help);
-      right->printMIPS(reg, out, help);
-      out << "SUBU " << reg << ", " << r1 << ", " << reg << std::endl;
-      help.regFlag[std::stoi(r1.substr(1))] = 0;
+        std::string r1 = "$" + std::to_string(help.findreg());
+        left->printMIPS(r1, out, help);
+        std::string r2 = "$" + std::to_string(help.findreg());
+        right->printMIPS(r2, out, help);
+        out << "SUBU " << reg << ", " << r1 << ", " << r2 << std::endl;
+        help.regFlag[std::stoi(r1.substr(1))] = 0;
+        help.regFlag[std::stoi(r2.substr(1))] = 0;
     }
     virtual void printC (std::ostream &out) const override{
         out << "( ";
