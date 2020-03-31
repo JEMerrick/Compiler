7204748b59e81d105ff097db2e2fd4eba9c6501b
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-03-31 15:49:29 +0100

    f

--- include/ast/ast_funcs.hpp
+++ include/ast/ast_funcs.hpp
@@ -172,13 +172,14 @@ public:
     }
 
     virtual void printMIPS (std::string reg, std::ostream &out, MIPZ &help) const override{
-        if(nextArg == NULL){
-            help.createlocal(id);
-        }
+        // if(nextArg == NULL){
+        //     help.createlocal(id);
+        // }
         if(nextArg!= NULL){
             nextArg->printMIPS(reg, out, help);
         }
-        out << "SW $" << std::to_string((help.parameters++)+4) << ", " << (help.createlocal(id))-4 << "($fp)" << std::endl;
+        out << "SW $" << std::to_string((help.parameters++)+4) << ", " << (help.createlocal(id)) << "($fp)" << std::endl;
+        //out << "SW $" << std::to_string((help.parameters++)+4) << ", " << (help.createlocal(id))-4 << "($fp)" << std::endl;
     }
     virtual void printC (std::ostream &out) const override{
         if(nextArg != NULL){
